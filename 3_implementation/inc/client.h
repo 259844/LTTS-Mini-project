f16b3149befab165ae9930c8b4df1104b190f16a
259844 <80749311+259844@users.noreply.github.com>
2021-04-16 14:12:45 +0530

    Update client.h

--- 3_implementation/inc/client.h
+++ 3_implementation/inc/client.h
@@ -15,3 +15,34 @@
 #define KCYN  "\x1B[36m"
 #define KWHT  "\x1B[37m"
 #define RESET "\033[0m"
+
+void
+trim_newline (char *text)
+  
+  
+  void
+get_username (char *username)
+  
+  
+  void
+set_username (connection_info * connection)
+
+  
+  void
+stop_client (connection_info * connection)
+  
+  
+  void
+connect_to_server (connection_info * connection, char *address, char *port)
+  
+  
+  void
+handle_user_input (connection_info * connection)
+  
+  
+  
+  void
+handle_server_message (connection_info * connection)
+  
+  
+  
