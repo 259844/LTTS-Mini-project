bab8d209d5e5f18df85e72d07e94a8637136009f
259844 <80749311+259844@users.noreply.github.com>
2021-04-16 12:19:48 +0530

    Update client.h

--- 3_implementation/inc/client.h
+++ 3_implementation/inc/client.h
@@ -1,1 +1,17 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\033[0m"
