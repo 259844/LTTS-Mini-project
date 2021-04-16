8db061c2bc9dac30a4d84cdd44f604409790bed7
259844 <80749311+259844@users.noreply.github.com>
2021-04-16 10:27:30 +0530

    Update prototype.h

--- 3_implementation/inc/prototype.h
+++ 3_implementation/inc/prototype.h
@@ -1,10 +1,21 @@
+/**
+ * @file prototype.h
+ * @author Yash Trivedi (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 ***************** Client's side ********************
 
-#include<stdio.h>
-#include<string.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
-#include<stdlib.h>
-#include<unistd.h>
+ #include<stdio.h>
+ #include<string.h>
+ #include<sys/types.h>
+ #include<sys/socket.h>
+ #include<netinet/in.h>
+ #include<arpa/inet.h>
+ #include<stdlib.h>
+ #include<unistd.h>
