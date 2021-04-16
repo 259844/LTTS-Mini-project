f75f97f1dd49779cb0672b5e94ca6179d23f9374
259844 <80749311+259844@users.noreply.github.com>
2021-04-16 10:25:17 +0530

    Update prototype.h

--- 3_implementation/inc/prototype.h
+++ 3_implementation/inc/prototype.h
@@ -1,10 +1,10 @@
 ***************** Client's side ********************
 
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<unistd.h>
