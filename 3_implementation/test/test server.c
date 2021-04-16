88aa93641e8942bef9524cf5087dd269b3c886e9
259844 <80749311+259844@users.noreply.github.com>
2021-04-16 16:17:24 +0530

    Update test server.c

--- 3_implementation/test/test server.c
+++ 3_implementation/test/test server.c
@@ -6,73 +6,30 @@
 /* Required by the unity test framework */
 void setUp(){}
 /* Required by the unity test framework */
-void tearDown(){}
+void tearDown()
 
 void
-trim_newline (char *text)
+test_trim_newline ()
 {
-    int len = strlen (text) - 1;
-    if (text[len] == '\n')
-      {
-          text[len] = '\0';
-      }
+   TEST_ASSERT_EQUAL(-1,text[len]("/n);
+
 }
 
 void
-send_public_message (connection_info clients[], int sender, char *message_text)
+test_send_public_message (connection_info clients[], int sender, char *message_text)
 {
-    message msg;
-    msg.type = PUBLIC_MESSAGE;
-    strncpy (msg.username, clients[sender].username, 20);
-    strncpy (msg.data, message_text, 256);
-    int i = 0;
-    for (i = 0; i < MAX_CLIENTS; i++)
-      {
-          if (i != sender && clients[i].socket != 0)
-            {
-                if (send (clients[i].socket, &msg, sizeof (msg), 0) < 0)
-                  {
-                      perror ("Send failed");
-                      exit (1);
-                  }
-            }
-      }
+   TEST_ASSERT_EQUAL(-1, i != sender && clients[i].socket != 0)
 }
 
 void
-send_connect_message (connection_info * clients, int sender)
+test_send_connect_message (connection_info * clients, int sender)
 {
-    message msg;
-    msg.type = CONNECT;
-    strncpy (msg.username, clients[sender].username, 21);
-    int i = 0;
-    for (i = 0; i < MAX_CLIENTS; i++)
-      {
-          if (clients[i].socket != 0)
-            {
-                if (i == sender)
-                  {
-                      msg.type = SUCCESS;
-                      if (send (clients[i].socket, &msg, sizeof (msg), 0) < 0)
-                        {
-                            perror ("Send failed");
-                            exit (1);
-                        }
-                  }
-                else
-                  {
-                      if (send (clients[i].socket, &msg, sizeof (msg), 0) < 0)
-                        {
-                            perror ("Send failed");
-                            exit (1);
-                        }
-                  }
-            }
+    TEST_ASSERT_EQUAL(-1,clients[i].socket != 0)
       }
     UNITY_BEGIN();
-    RUN_TEST(trim_newline);
-    RUN_TEST(send public message);
-    RUN_TEST(send connect message);
+    RUN_TEST(test_trim_newline);
+    RUN_TEST(test_send_public_message);
+    RUN_TEST(test_send_connect =_message);
 
     return UNITY_END();
 }
