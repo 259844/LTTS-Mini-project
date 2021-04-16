c7da40c0e64466189efeb9a24341c786638dd447
259844 <80749311+259844@users.noreply.github.com>
2021-04-16 15:48:21 +0530

    Update test.c

--- 3_implementation/test/test.c
+++ 3_implementation/test/test.c
@@ -1,3 +1,19 @@
 #include<stdio.h>
 #include "../unity/unity.h"
 #include "../unity/unity_internals.h"
+#include "/inc/server.h"
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+void
+trim_newline (char *text)
+{
+    int len = strlen (text) - 1;
+    if (text[len] == '\n')
+      {
+          text[len] = '\0';
+      }
+}
